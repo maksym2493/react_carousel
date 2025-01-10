4947fb7f297519dbd9e5b38df6d95c0c2a575b39
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2025-01-10 22:46:35 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.scss';
+
 import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+
+  step: number;
+  animationDuration: number;
+
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +28,100 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+
+    step: 3,
+    animationDuration: 1000,
+
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, itemWidth, frameSize, step, animationDuration, infinite } =
+      this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form className="form">
+          <label htmlFor="itemId" className="form__field">
+            Item width:
+            <input
+              type="number"
+              id="itemId"
+              name="itemWidth"
+              min={50}
+              value={itemWidth}
+              onChange={event =>
+                this.setState({ itemWidth: +event.currentTarget.value })
+              }
+            />
+          </label>
+
+          <label htmlFor="frameId" className="form__field">
+            Frame size:
+            <input
+              type="number"
+              id="frameId"
+              name="frameSize"
+              value={frameSize}
+              min="1"
+              max={images.length - step}
+              onChange={event =>
+                this.setState({ frameSize: +event.currentTarget.value })
+              }
+            />
+          </label>
+
+          <label htmlFor="stepId" className="form__field">
+            Step:
+            <input
+              type="number"
+              id="stepId"
+              name="step"
+              value={step}
+              min={1}
+              max={images.length - frameSize}
+              onChange={event =>
+                this.setState({ step: +event.currentTarget.value })
+              }
+            />
+          </label>
+
+          <label className="form__field">
+            Animation duration:
+            <input
+              type="number"
+              name="animationDuration"
+              min={50}
+              value={animationDuration}
+              onChange={event =>
+                this.setState({ animationDuration: +event.currentTarget.value })
+              }
+            />
+          </label>
+
+          <label className="form__checkbox">
+            Infinite:
+            <input
+              type="checkbox"
+              name="inifinite"
+              checked={infinite}
+              onChange={() => this.setState({ infinite: !infinite })}
+            />
+          </label>
+        </form>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
